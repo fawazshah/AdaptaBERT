0bba3e7b5e66db9912b9032175069c0d37e6c9b3
Fawaz Shah <fawaz.shah17@imperial.ac.uk>
2021-05-14 02:07:27 +0100

    Use new fused adam instead of old FP16_Optimizer. Remove half() and upgrade to use amp

--- NER/domain-tuning.py
+++ NER/domain-tuning.py
@@ -486,8 +486,6 @@ def main():
         model = MyBertForMaskedLM.from_pretrained(args.trained_model_dir, state_dict=previous_state_dict)
     else:
         model = MyBertForMaskedLM.from_pretrained(args.bert_model)
-    if args.fp16:
-        model.half()
     model.to(device)
     if args.local_rank != -1:
         try:
@@ -508,19 +506,16 @@ def main():
 
     if args.fp16:
         try:
-            from apex.optimizers import FP16_Optimizer
             from apex.optimizers import FusedAdam
+            from apex import amp
         except ImportError:
             raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use distributed and fp16 training.")
 
         optimizer = FusedAdam(optimizer_grouped_parameters,
                               lr=args.learning_rate,
-                              bias_correction=False,
-                              max_grad_norm=1.0)
-        if args.loss_scale == 0:
-            optimizer = FP16_Optimizer(optimizer, dynamic_loss_scale=True)
-        else:
-            optimizer = FP16_Optimizer(optimizer, static_loss_scale=args.loss_scale)
+                              bias_correction=False)
+
+        model, optimizer = amp.initialize(model, optimizer, opt_level="O2")
 
     else:
         optimizer = BertAdam(optimizer_grouped_parameters,
@@ -556,7 +551,8 @@ def main():
                 if args.gradient_accumulation_steps > 1:
                     loss = loss / args.gradient_accumulation_steps
                 if args.fp16:
-                    optimizer.backward(loss)
+                    with amp.scale_loss(loss, optimizer) as scaled_loss:
+                      scaled_loss.backward()
                 else:
                     loss.backward()
                 tr_loss += loss.item()
