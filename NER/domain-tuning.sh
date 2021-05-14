3c988eb5b6cf9ee66e674a097609ff7c436af1e2
Fawaz Shah <fawaz.shah17@imperial.ac.uk>
2021-05-14 02:09:03 +0100

    Add script to submit to gpucluster

---
+++ NER/domain-tuning.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL # required to send email notifcations
+#SBATCH --mail-user=fs2217 # required to send email notifcations - please replace <your_username> with your college login name or email address
+export PATH=/vol/bitbucket/fs2217/miniconda3/bin/:$PATH
+source activate
+source /vol/cuda/11.1.0-cudnn8.0.4.30/setup.sh
+TERM=vt100 # or TERM=xterm
+
+python3 -W ignore domain-tuning.py --data_dir="data/" --bert_model="bert-base-cased" --output_dir="lm_output/" --max_seq_length=128 --do_train --train_batch_size=64 --learning_rate=5e-5 --num_train_epochs=3 --warmup_proportion=0.1 --seed=2019 --fp16
+echo Done!
