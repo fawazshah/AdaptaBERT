51c43dfe2e8a748f11cc76b9401462f145f956fc
Fawaz Shah <fawaz010@gmail.com>
2021-05-15 12:01:49 +0100

    Add unpickling script

---
+++ NER/data/unpickle.py
@@ -0,0 +1,8 @@
+import pickle
+
+def _read_pkl(input_file):
+    """Reads a tab separated value file."""
+    data = pickle.load(open(input_file, 'rb'))
+    return data
+
+print(_read_pkl('conll_train.pkl')[1])
