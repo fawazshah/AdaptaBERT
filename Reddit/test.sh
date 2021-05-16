f972a32560d841eee2ac8fe5b92176ca2843b0c5
Fawaz Shah <fawaz010@gmail.com>
2021-05-16 20:07:51 +0100

    Create new Reddit section

---
+++ Reddit/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL # required to send email notifcations
+#SBATCH --mail-user=fs2217 # required to send email notifcations - please replace <your_username> with your college login name or email address
+export PATH=/vol/bitbucket/fs2217/miniconda3/bin/:$PATH
+source activate
+source /vol/cuda/11.1.0-cudnn8.0.4.30/setup.sh
+TERM=vt100 # or TERM=xterm
+
+python3 -W ignore test.py --data_dir="data/" --bert_model="bert-base-cased" --output_dir="error_analysis_output/" --trained_model_dir="trained_model/" --max_seq_length=128 --do_test --eval_batch_size=1 --seed=2019
+echo Done!
