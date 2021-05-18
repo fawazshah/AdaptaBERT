13b6c263d9fd1e7f43e9e2b14dd37d4999defe8c
Fawaz Shah <fawaz010@gmail.com>
2021-05-18 20:28:25 +0100

    Change usages of DataProcessor to use src and trg

--- Reddit/test.py
+++ Reddit/test.py
@@ -328,7 +328,7 @@ def main():
     model.to(device)
 
     if args.do_test and (args.local_rank == -1 or torch.distributed.get_rank() == 0):
-        test_examples = processor.get_sep_twitter_test_examples(args.data_dir)
+        test_examples = processor.get_trg_test_examples(args.data_dir)
         test_features = convert_examples_to_features(
             test_examples, label_list, args.max_seq_length, tokenizer)
         logger.info("***** Running final test *****")
