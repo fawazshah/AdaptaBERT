b8995ee49ac9b4e242c1d0abc8880f6cadc48964
Fawaz Shah <fawaz010@gmail.com>
2021-05-18 16:51:49 +0100

    Change all_logits to all_predictions. Remove unneeded funcs.

--- Reddit/task-tuning.py
+++ Reddit/task-tuning.py
@@ -46,69 +46,6 @@ logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(messa
 logger = logging.getLogger(__name__)
 
 
-class MyBertForTokenClassification(BertPreTrainedModel):
-    """BERT model for token-level classification.
-    This module is composed of the BERT model with a linear layer on top of
-    the full hidden state of the last layer.
-    Params:
-        `config`: a BertConfig class instance with the configuration to build a new model.
-        `num_labels`: the number of classes for the classifier. Default = 2.
-    Inputs:
-        `input_ids`: a torch.LongTensor of shape [batch_size, sequence_length]
-            with the word token indices in the vocabulary(see the tokens preprocessing logic in the scripts
-            `extract_features.py`, `run_classifier.py` and `run_squad.py`)
-        `token_type_ids`: an optional torch.LongTensor of shape [batch_size, sequence_length] with the token
-            types indices selected in [0, 1]. Type 0 corresponds to a `sentence A` and type 1 corresponds to
-            a `sentence B` token (see BERT paper for more details).
-        `attention_mask`: an optional torch.LongTensor of shape [batch_size, sequence_length] with indices
-            selected in [0, 1]. It's a mask to be used if the input sequence length is smaller than the max
-            input sequence length in the current batch. It's the mask that we typically use for attention when
-            a batch has varying length sentences.
-        `labels`: labels for the classification output: torch.LongTensor of shape [batch_size, sequence_length]
-            with indices selected in [0, ..., num_labels].
-    Outputs:
-        if `labels` is not `None`:
-            Outputs the CrossEntropy classification loss of the output with the labels.
-        if `labels` is `None`:
-            Outputs the classification logits of shape [batch_size, sequence_length, num_labels].
-    Example usage:
-    ```python
-    # Already been converted into WordPiece token ids
-    input_ids = torch.LongTensor([[31, 51, 99], [15, 5, 0]])
-    input_mask = torch.LongTensor([[1, 1, 1], [1, 1, 0]])
-    token_type_ids = torch.LongTensor([[0, 0, 1], [0, 1, 0]])
-    config = BertConfig(vocab_size_or_config_json_file=32000, hidden_size=768,
-        num_hidden_layers=12, num_attention_heads=12, intermediate_size=3072)
-    num_labels = 2
-    model = BertForTokenClassification(config, num_labels)
-    logits = model(input_ids, token_type_ids, input_mask)
-    ```
-    """
-    def __init__(self, config, num_labels):
-        super(MyBertForTokenClassification, self).__init__(config)
-        self.num_labels = num_labels
-        self.bert = BertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Linear(config.hidden_size, num_labels)
-        self.apply(self.init_bert_weights)
-
-    def forward(self, input_ids, token_type_ids, attention_mask, labels=None, label_mask=None):
-        sequence_output, _ = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
-        sequence_output = self.dropout(sequence_output)
-        logits = self.classifier(sequence_output)
-
-        if labels is not None:
-            loss_fct = CrossEntropyLoss()
-            # Only keep active parts of the loss
-            active_loss = label_mask.view(-1) == 1
-            active_logits = logits.view(-1, self.num_labels)[active_loss]
-            active_labels = labels.view(-1)[active_loss]
-            loss = loss_fct(active_logits, active_labels)
-            return loss
-        else:
-            return logits
-
-
 class InputExample(object):
     """A single training/test example for simple sequence classification."""
 
@@ -251,84 +188,6 @@ def convert_examples_to_features(examples, max_seq_length, tokenizer):
                               label=label))
     return features
 
-def accuracy(out, label_ids, label_mask):
-    # axis-0: seqs in batch; axis-1: toks in seq; axis-2: potential labels of tok
-    outputs = np.argmax(out, axis=2)
-    matched = outputs == label_ids
-    num_correct = np.sum(matched * label_mask)
-    num_total = np.sum(label_mask)
-    return num_correct, num_total
-
-def true_and_pred(out, label_ids, label_mask):
-    # axis-0: seqs in batch; axis-1: toks in seq; axis-2: potential labels of tok
-    tplist = []
-    outputs = np.argmax(out, axis=2)
-    for i in range(len(label_ids)):
-        trues = []
-        preds = []
-        for true, pred, mask in zip(label_ids[i], outputs[i], label_mask[i]):
-            if mask:
-                trues.append(true)
-                preds.append(pred)
-        tplist.append((trues, preds))
-    return tplist
-
-def compute_tfpn(_trues, _preds, label_map):
-    trues = _trues + [label_map['O']]
-    preds = _preds + [label_map['O']]
-
-    true_ent_list = []
-    pred_ent_list = []
-
-    ent_start = -1
-    for i, label in enumerate(trues):
-        if ent_start == -1:
-            if label == label_map['B']:
-                ent_start = i
-            elif label == label_map['I']:
-                assert 0 == 1 # should not occur in ground truth
-        else:
-            if label == label_map['B']:
-                true_ent_list.append((ent_start, i))
-                ent_start = i
-            elif label == label_map['O']:
-                true_ent_list.append((ent_start, i))
-                ent_start = -1
-
-    ent_start = -1
-    for i, label in enumerate(preds):
-        if ent_start == -1:
-            if label == label_map['B']:
-                ent_start = i
-            elif label == label_map['I']:
-                ent_start = i # entities in prediction might start with "I"
-        else:
-            if label == label_map['B']:
-                pred_ent_list.append((ent_start, i))
-                ent_start = i
-            elif label == label_map['O']:
-                pred_ent_list.append((ent_start, i))
-                ent_start = -1
-
-    TP, FP, FN, _TP = 0, 0, 0, 0
-    for ent in true_ent_list:
-        if ent in pred_ent_list:
-            TP += 1
-        else:
-            FN += 1
-    for ent in pred_ent_list:
-        if ent in true_ent_list:
-            _TP += 1
-        else:
-            FP += 1
-    assert TP == _TP
-    return TP, FP, FN
-
-def compute_f1(TP, FP, FN):
-    P = TP / (TP + FP)
-    R = TP / (TP + FN)
-    F1 = 2 * P * R / (P + R)
-    return "Precision: " + str(P) + ", Recall: " + str(R) + ", F1: " + str(F1)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -682,7 +541,7 @@ def main():
         test_loss = 0
         nb_test_steps = 0
 
-        all_logits = []
+        all_predictions = []
         all_labels = []
 
         for input_ids, input_mask, segment_ids, labels in tqdm(test_dataloader, desc="Testing"):
@@ -698,15 +557,15 @@ def main():
             predictions = np.argmax(logits.detach().cpu().numpy(), axis=1)
             labels = labels.to('cpu').numpy()
 
-            all_logits.extend(predictions)
+            all_predictions.extend(predictions)
             all_labels.extend(labels)
 
             test_loss += tmp_test_loss.mean().item()
             nb_test_steps += 1
 
         test_loss = test_loss / nb_test_steps
-        test_accuracy = accuracy_score(all_labels, all_logits)
-        test_f1 = f1_score(all_labels, all_logits, average='macro')
+        test_accuracy = accuracy_score(all_labels, all_predictions)
+        test_f1 = f1_score(all_labels, all_predictions, average='macro')
         result = {'test_loss': test_loss,
                   'test_accuracy': test_accuracy,
                   'test_f1': test_f1}
