79484cd8dff5c8c38d3d0a73fb8af256d0b98299
Fawaz Shah <fawaz010@gmail.com>
2021-06-03 01:26:59 +0100

    NER add plot of num sentences in tweets

--- NER/explore-data.py
+++ NER/explore-data.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import os
+import pandas as pd
 import pickle
 import re
 
@@ -13,8 +15,6 @@ train_data = _read_pkl(os.path.join(data_dir, "sep_twitter_train.pkl"))
 test_data = _read_pkl(os.path.join(data_dir, "sep_twitter_test.pkl"))
 all_data = train_data + test_data
 
-print(f'Number of Twitter samples: {len(all_data)}')
-
 num_sentences = []
 
 for elem in all_data:
@@ -25,4 +25,13 @@ for elem in all_data:
     sentences = [t.strip() for t in split_regex.split(text) if t.strip() != '']
     num_sentences.append(len(sentences))
 
-print(f'Avg. Twitter num sentences: {sum(num_sentences) / len(num_sentences)}')
+print('Twitter num sentences information:')
+
+num_sentences = pd.Series(num_sentences)
+print(num_sentences.describe())
+num_sentences.sort_values(inplace=True)
+num_sentences.reset_index(drop=True, inplace=True)
+num_sentences.plot(fontsize=14, figsize=(10, 7))
+plt.xlabel('tweet number', fontsize=16)
+plt.ylabel('no. sentences', fontsize=16)
+plt.show()
