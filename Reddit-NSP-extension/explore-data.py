150aafab21f2b9e2c114101094d416c32d4e92f3
Fawaz Shah <fawaz010@gmail.com>
2021-06-05 17:32:49 +0100

    Zoom into num sentences graphs Reddit

--- Reddit-NSP-extension/explore-data.py
+++ Reddit-NSP-extension/explore-data.py
@@ -6,7 +6,7 @@ import re
 
 data_dir = 'data/train-test-split'
 
-# SUBMISSIONS
+# ARTICLES
 
 submissions_train = pd.read_csv(os.path.join(data_dir, 'submissions_train.tsv'), sep='\t')
 submissions_test = pd.read_csv(os.path.join(data_dir, 'submissions_test.tsv'), sep='\t')
@@ -31,7 +31,7 @@ num_sentences_submissions_dist = num_sentences_submissions.value_counts()
 num_sentences_submissions_dist.sort_index(inplace=True)
 num_sentences_submissions_dist = num_sentences_submissions_dist.to_frame('frequency')
 # Remove small outliers for easier visualisation
-num_sentences_submissions_dist.drop(num_sentences_submissions_dist[num_sentences_submissions_dist.index > 150].index, inplace=True)
+num_sentences_submissions_dist.drop(num_sentences_submissions_dist[num_sentences_submissions_dist.index > 100].index, inplace=True)
 # Make index 'continuous'
 num_sentences_submissions_dist = num_sentences_submissions_dist.reindex(range(num_sentences_submissions_dist.index[-1]+1)).fillna(0)
 num_sentences_submissions_dist.reset_index(inplace=True)
@@ -39,7 +39,7 @@ num_sentences_submissions_dist.reset_index(inplace=True)
 num_sentences_submissions_dist.plot(x='index', y='frequency', kind='bar', fontsize=14, figsize=(10, 7), legend=None)
 plt.xlabel('no. sentences in text', fontsize=16)
 plt.ylabel('frequency', fontsize=16)
-plt.xticks(np.arange(0, 151, 25), rotation=0)
+plt.xticks(np.arange(0, 101, 20), rotation=0)
 plt.title('articles', fontsize=24)
 plt.show()
 
@@ -68,7 +68,7 @@ num_sentences_comments_dist = num_sentences_comments.value_counts()
 num_sentences_comments_dist.sort_index(inplace=True)
 num_sentences_comments_dist = num_sentences_comments_dist.to_frame('frequency')
 # Remove small outliers for easier visualisation
-num_sentences_comments_dist.drop(num_sentences_comments_dist[num_sentences_comments_dist.index > 25].index, inplace=True)
+num_sentences_comments_dist.drop(num_sentences_comments_dist[num_sentences_comments_dist.index > 15].index, inplace=True)
 # Make index 'continuous'
 num_sentences_comments_dist = num_sentences_comments_dist.reindex(range(num_sentences_comments_dist.index[-1]+1)).fillna(0)
 num_sentences_comments_dist.reset_index(inplace=True)
@@ -76,6 +76,6 @@ num_sentences_comments_dist.reset_index(inplace=True)
 num_sentences_comments_dist.plot(x='index', y='frequency', kind='bar', fontsize=14, figsize=(10, 7), legend=None)
 plt.xlabel('no. sentences in text', fontsize=16)
 plt.ylabel('frequency', fontsize=16)
-plt.xticks(np.arange(0, 26, 5), rotation=0)
+plt.xticks(np.arange(0, 16, 5), rotation=0)
 plt.title('comments', fontsize=24)
 plt.show()
