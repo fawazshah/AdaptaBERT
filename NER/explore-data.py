916f70865dbaf5be18e96f453782f276618d4f30
Fawaz Shah <fawaz010@gmail.com>
2021-06-05 21:05:11 +0100

    Reduce scale of NER articles graph

--- NER/explore-data.py
+++ NER/explore-data.py
@@ -37,7 +37,7 @@ num_sentences_articles_dist = num_sentences_articles.value_counts()
 num_sentences_articles_dist.sort_index(inplace=True)
 num_sentences_articles_dist = num_sentences_articles_dist.to_frame('frequency')
 # Remove small outliers for easier visualisation
-num_sentences_articles_dist.drop(num_sentences_articles_dist[num_sentences_articles_dist.index > 15].index, inplace=True)
+num_sentences_articles_dist.drop(num_sentences_articles_dist[num_sentences_articles_dist.index > 6].index, inplace=True)
 # Make index 'continuous'
 num_sentences_articles_dist = num_sentences_articles_dist.reindex(range(num_sentences_articles_dist.index[-1]+1)).fillna(0)
 num_sentences_articles_dist.reset_index(inplace=True)
@@ -45,7 +45,7 @@ num_sentences_articles_dist.reset_index(inplace=True)
 num_sentences_articles_dist.plot(x='index', y='frequency', kind='bar', fontsize=14, figsize=(10, 7), legend=None)
 plt.xlabel('no. sentences in text', fontsize=16)
 plt.ylabel('frequency', fontsize=16)
-plt.xticks(np.arange(1, 50, 5), rotation=0)
+plt.xticks(np.arange(1, 7), rotation=0)
 plt.title('articles', fontsize=24)
 plt.show()
 
